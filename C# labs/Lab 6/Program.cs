4b7eaf3f4e5dc2a22ffbbc774fd31ebb5c5f9139
MMansyy <hessenmansyy@gmail.com>
2026-02-07 14:40:49 +0200

    add Lab 6 and Lab 7

---
+++ C# labs/Lab 6/Program.cs
@@ -0,0 +1,180 @@
+﻿using static Lab_6.Program;
+using static Lab_6.TemperatureSensor.Program;
+
+namespace Lab_6
+{
+    public delegate double MathOp(double x, double y);
+
+    public delegate void NotifyHandler(string s);
+
+    public delegate bool IntFilter(int value);
+
+    public delegate void TemperatureHandler(string msg, double value);
+    class Calc
+    {
+        public static double Add(double x, double y)
+        {
+            return x + y;
+        }
+        public static double Sub(double x, double y)
+        {
+            return x - y;
+        }
+        public static double Mul(double x, double y)
+        {
+            return x * y;
+        }
+        public static double Div(double x, double y)
+        {
+            try
+            {
+                return x / y;
+            }
+            catch (DivideByZeroException e)
+            {
+                throw new Exception("Cant divide by zero ya ahbl");
+            }
+        }
+    }
+
+
+
+    class TemperatureSensor
+    {
+        public event TemperatureHandler TemperatureHigh;
+
+        public void SetTemperature(double temp)
+        {
+            if (temp > 30)
+            {
+                if (TemperatureHigh != null)
+                {
+                    TemperatureHigh("Danger! It's hot!", temp);
+                }
+            }
+        }
+    }
+
+    public class TemperatureMonitor
+    {
+        public void OnHighTemperature(string msg, double temp)
+        {
+            Console.WriteLine($"[Monitor Alert]: {msg} (Current Temp: {temp})");
+        }
+    }
+    internal class Program
+    {
+
+        public static void SendEmail(string s)
+        {
+            Console.WriteLine(s + " From Send Email");
+        }
+        public static void SendSMS(string s)
+        {
+            Console.WriteLine(s + " From Send SMS");
+        }
+
+        public static List<int> FilterArray(int[] array, IntFilter filter)
+        {
+            List<int> list = new List<int>();
+            foreach (var item in array)
+            {
+                if (filter(item))
+                {
+                    list.Add(item);
+                }
+            }
+            return list;
+        }
+
+        public static bool IsEven(int value)
+        {
+            return value % 2 == 0;
+        }
+
+        public static bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
+
+        public static bool isGreaterThan2(int x)
+        {
+            return x > 2;
+        }
+        static void Main(string[] args)
+        {
+            // task 1
+            MathOp op = Calc.Add;
+
+            var res = op(5, 7);
+            Console.WriteLine(res);
+
+            op = Calc.Mul;
+            res = op(5, 7);
+            Console.WriteLine(res);
+            Console.WriteLine("-------------------------------------------------------------------");
+            // task 2
+            NotifyHandler notify = Program.SendEmail;
+            notify("Send it Succufully");
+
+            notify += Program.SendSMS;
+            notify("Send it Succufully");
+
+            notify -= Program.SendEmail;
+
+            notify("Send it Succufully");
+            Console.WriteLine("-------------------------------------------------------------------");
+
+            // task 3
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var even = FilterArray(numbers, IsEven);
+            foreach (var item in even)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("---------");
+            var odd = FilterArray(numbers, IsOdd);
+            foreach (var item in odd)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-------------------------------------------------------------------");
+
+            // task 4
+            even = FilterArray(numbers, delegate (int x) { return x % 2 == 0; });
+            foreach (var item in even)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-------------------------------------------------------------------");
+
+            // task 5 & 6
+            List<int> numbers2 = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            numbers2.Sort((a, b) => b.CompareTo(a));
+            numbers2.ForEach(x => Console.WriteLine(x));
+            Console.WriteLine("-----------");
+            var x = numbers2.FindAll(x => x % 2 == 0);
+            x.ForEach(x => Console.WriteLine(x));
+
+
+            // task 7
+            TemperatureSensor sensor = new TemperatureSensor();
+            TemperatureMonitor monitor = new TemperatureMonitor();
+
+            sensor.TemperatureHigh += monitor.OnHighTemperature;
+
+            sensor.SetTemperature(20);
+
+            Console.WriteLine("----------------");
+
+            sensor.SetTemperature(35);
+
+            Console.WriteLine("----------------");
+
+            sensor.TemperatureHigh -= monitor.OnHighTemperature;
+
+            sensor.SetTemperature(45); 
+        }
+    }
+}
---
+++ C# labs/Lab 7/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System.Drawing.Drawing2D;
+
+namespace Lab_7
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+             g = e.Graphics;
+            // العناوين
+            Font companyFont = new Font("Arial", 20, FontStyle.Bold);
+            Font titleFont = new Font("Arial", 14, FontStyle.Regular);
+            Brush brush1 = Brushes.Black;
+            g.DrawString("ABC Company", companyFont, brush1, new PointF(250, 30));
+            g.DrawString("Annual Revenue", titleFont, brush1, new PointF(270, 70));
+
+            // الجدول
+            int tableX = 400;
+            int tableY = 120;
+            int tableWidth = 350;
+            int tableHeight = 400;
+
+            Pen pen = new Pen(Color.Black, 2);
+            g.DrawRectangle(pen, tableX, tableY, tableWidth, tableHeight);
+
+            // تقسيم الجدول من النص
+            int columnX = tableX + (tableWidth / 2);
+
+            g.DrawLine(
+                pen,
+                columnX,
+                tableY,
+                columnX,
+                tableY + tableHeight
+            );
+
+            // تقسيم الجدول بالعرض
+            int rowCount = 11;
+            int rowHeight = tableHeight / rowCount;
+
+            for (int i = 1; i < rowCount; i++)
+            {
+                int y = tableY + i * rowHeight;
+                g.DrawLine(pen, tableX, y, tableX + tableWidth, y);
+            }
+
+            //الداتا بتاعت الجدول
+            Font headerFont = new Font("Arial", 12, FontStyle.Bold);
+            Font dataFont = new Font("Arial", 12, FontStyle.Regular);
+            Brush brush = Brushes.Black;
+
+            g.DrawString("Year", headerFont, brush, new PointF(tableX + 10, tableY + 10));
+            g.DrawString("Revenue", headerFont, brush, new PointF(columnX + 10, tableY + 10));
+
+            int[] years = { 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997 };
+            int[] revenues = { 150, 170, 180, 175, 200, 250, 210, 240, 280, 140 };
+
+            for (int i = 0; i < years.Length; i++)
+            {
+                int yPosition = tableY + (i + 1) * rowHeight + 10; 
+                g.DrawString(years[i].ToString(), dataFont, brush, new PointF(tableX + 10, yPosition));
+                g.DrawString(revenues[i].ToString(), dataFont, brush, new PointF(columnX + 10, yPosition));
+            }
+            // تشارت
+            int chartX = 30;  // اليسار
+            int chartY = 120; // أعلى
+            int chartWidth = 350;
+            int chartHeight = 400;
+            // قلم أسود للمحاور
+            Pen axisPen = new Pen(Color.Black, 2);
+
+            // Y-axis
+            e.Graphics.DrawLine(axisPen, chartX, chartY, chartX, chartY + chartHeight);
+            // X-axis
+            e.Graphics.DrawLine(axisPen, chartX, chartY + chartHeight, chartX + chartWidth, chartY + chartHeight);
+
+            int maxRevenue = revenues.Max(); // 280
+            float scaleY = (float)chartHeight / maxRevenue;
+
+            int barWidth = 20;
+            int gap = 15; // المسافة بين الأعمدة
+
+            for (int i = 0; i < years.Length; i++)
+            {
+                int x = chartX + gap + i * (barWidth + gap);
+                int y = chartY + chartHeight - (int)(revenues[i] * scaleY); // من الأسفل للأعلى
+                int height = (int)(revenues[i] * scaleY);
+
+                // قلم فرشاة Hatch
+                HatchBrush brush2 = new HatchBrush(HatchStyle.ForwardDiagonal, Color.Red, Color.White);
+                e.Graphics.FillRectangle(brush2, x, y, barWidth, height);
+                e.Graphics.DrawRectangle(Pens.Black, x, y, barWidth, height); // حدود العمود
+            }
+
+            Pen linePen = new Pen(Color.Blue, 2);
+
+            for (int i = 0; i < years.Length - 1; i++)
+            {
+                int x1 = chartX + gap + i * (barWidth + gap) + barWidth / 2;
+                int y1 = chartY + chartHeight - (int)(revenues[i] * scaleY);
+
+                int x2 = chartX + gap + (i + 1) * (barWidth + gap) + barWidth / 2;
+                int y2 = chartY + chartHeight - (int)(revenues[i + 1] * scaleY);
+
+                e.Graphics.DrawLine(linePen, x1, y1, x2, y2);
+            }
+
+            Font font = new Font("Arial", 9);
+            for (int i = 0; i < years.Length; i++)
+            {
+                int x = chartX + gap + i * (barWidth + gap) + barWidth / 2 - 10;
+                int y = chartY + chartHeight + 5;
+                e.Graphics.DrawString(years[i].ToString(), font, Brushes.Black, x, y);
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
